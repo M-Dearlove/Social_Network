1bf2bf6c2044a690bcb2b8f9f96e955c6b0c2d34
M-Dearlove <dearlove.matthew@gmail.com>
2025-02-27 09:26:47 -0500

    configuration, models, and controllers.

---
+++ models/User.ts
@@ -0,0 +1,48 @@
+import { Schema, Document, model, ObjectId } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: ObjectId[];
+    friends: ObjectId[];
+};
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+});
+
+userSchema.virtual('friendCount').get(function (this: IUser) {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+export default User;
